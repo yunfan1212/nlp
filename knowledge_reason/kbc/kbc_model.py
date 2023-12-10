36c4d351630ca953d5f2ea1aab6a5190bd969bbd
云帆@ <66588855+yunfan1212@users.noreply.github.com>
2023-12-10 10:53:56 +0800

    Add files via upload

---
+++ knowledge_reason/kbc/kbc_model.py
@@ -0,0 +1,174 @@
+#coding=utf8
+
+
+
+'''kbc model
+https://paperswithcode.com/paper/canonical-tensor-decomposition-for-knowledge
+TRAIN:  {'MRR': 0.9995309710502625, 'hits@[1,3,10]': tensor([0.9992, 0.9999, 0.9999])}
+         VALID :  {'MRR': 0.8358657956123352, 'hits@[1,3,10]': tensor([0.7926, 0.8657, 0.9072])}
+'''
+
+from abc import ABC,abstractmethod
+from typing import Tuple,List,Dict
+import torch
+from torch import nn
+
+class KBCModel(nn.Module,ABC):
+
+    @abstractmethod
+    def get_rhs(self,chunk_begin:int,chunk_size:int):
+        pass
+
+    @abstractmethod
+    def get_queries(self, queries: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def score(self, x: torch.Tensor):
+        pass
+
+    def get_ranking(
+            self, queries: torch.Tensor,
+            filters: Dict[Tuple[int, int], List[int]],
+            batch_size: int = 1000, chunk_size: int = -1
+    ):
+        """
+        Returns filtered ranking for each queries.
+        :param queries: a torch.LongTensor of triples (lhs, rel, rhs)
+        :param filters: filters[(lhs, rel)] gives the rhs to filter from ranking
+        :param batch_size: maximum number of queries processed at once
+        :param chunk_size: maximum number of candidates processed at once
+        :return:
+        """
+        if chunk_size < 0:
+            chunk_size = self.sizes[2]            #所有节点数量
+        ranks = torch.ones(len(queries))
+        with torch.no_grad():
+            c_begin = 0
+            while c_begin < self.sizes[2]:
+                b_begin = 0
+                rhs = self.get_rhs(c_begin, chunk_size)      #右节点候选集
+                while b_begin < len(queries):
+                    these_queries = queries[b_begin:b_begin + batch_size]    #batch 数据 输入
+
+                    q = self.get_queries(these_queries)    #左节点+关系  信息
+                    scores = q @ rhs                        # 所有右节点得分，预测右节点得分,取前k个，为预测值
+
+                    targets = self.score(these_queries)        #目标右节点得分
+                    # set filtered and true scores to -1e6 to be ignored
+                    # take care that scores are chunked
+                    for i, query in enumerate(these_queries):         #遍历每一个三元组
+                        filter_out = filters[(query[0].item(), query[1].item())]     #目标节点集合：右节点集合
+                        filter_out += [queries[b_begin + i, 2].item()]               # 目标节点
+                        if chunk_size < self.sizes[2]:
+                            filter_in_chunk = [
+                                int(x - c_begin) for x in filter_out
+                                if c_begin <= x < c_begin + chunk_size
+                            ]
+                            scores[i, torch.LongTensor(filter_in_chunk)] = -1e6
+                        else:
+                            scores[i, torch.LongTensor(filter_out)] = -1e6          #正确集合 排除掉  预测得分   [batch,dims]
+                    ranks[b_begin:b_begin + batch_size] += torch.sum(               #错误的个数
+                        (scores >= targets).float(), dim=1
+                    ).cpu()
+
+                    b_begin += batch_size
+
+                c_begin += chunk_size
+        return ranks
+
+
+
+
+
+class ComplEx(KBCModel):
+    def __init__(
+            self, sizes: Tuple[int, int, int], rank: int,
+            init_size: float = 1e-3
+    ):
+        super(ComplEx, self).__init__()
+        self.sizes = sizes
+        self.rank = rank
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(s, 2 * rank, sparse=True)
+            for s in sizes[:2]
+        ])
+        self.embeddings[0].weight.data *= init_size
+        self.embeddings[1].weight.data *= init_size
+
+    def score(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        return torch.sum(
+            (lhs[0] * rel[0] - lhs[1] * rel[1]) * rhs[0] +
+            (lhs[0] * rel[1] + lhs[1] * rel[0]) * rhs[1],
+            1, keepdim=True
+        )
+
+    def forward(self, x):
+        lhs = self.embeddings[0](x[:, 0])
+        rel = self.embeddings[1](x[:, 1])
+        rhs = self.embeddings[0](x[:, 2])
+
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+        rhs = rhs[:, :self.rank], rhs[:, self.rank:]
+
+        to_score = self.embeddings[0].weight
+        to_score = to_score[:, :self.rank], to_score[:, self.rank:]
+        return (
+            (lhs[0] * rel[0] - lhs[1] * rel[1]) @ to_score[0].transpose(0, 1) +
+            (lhs[0] * rel[1] + lhs[1] * rel[0]) @ to_score[1].transpose(0, 1)
+        ), (
+            torch.sqrt(lhs[0] ** 2 + lhs[1] ** 2),
+            torch.sqrt(rel[0] ** 2 + rel[1] ** 2),
+            torch.sqrt(rhs[0] ** 2 + rhs[1] ** 2)
+        )
+
+    def get_rhs(self, chunk_begin: int, chunk_size: int):
+        return self.embeddings[0].weight.data[
+            chunk_begin:chunk_begin + chunk_size
+        ].transpose(0, 1)
+
+    def get_queries(self, queries: torch.Tensor):
+        lhs = self.embeddings[0](queries[:, 0])
+        rel = self.embeddings[1](queries[:, 1])
+        lhs = lhs[:, :self.rank], lhs[:, self.rank:]
+        rel = rel[:, :self.rank], rel[:, self.rank:]
+
+        return torch.cat([
+            lhs[0] * rel[0] - lhs[1] * rel[1],
+            lhs[0] * rel[1] + lhs[1] * rel[0]
+        ], 1)
+
+
+
+
+
+
+'''归一化'''
+class N3(nn.Module):
+    def __init__(self, weight: float):
+        super(N3, self).__init__()
+        self.weight = weight
+
+    def forward(self, factors):
+        norm = 0
+        for f in factors:
+            norm += self.weight * torch.sum(
+                torch.abs(f) ** 3
+            )
+        return norm / factors[0].shape[0]
+
+
+
+
+
+
